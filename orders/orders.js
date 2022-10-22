60e51a7a6633ddbfd7cf93e8690a0906cc792f46
t-tornado <anthonyamp0011@gmail.com>
2022-10-22 11:18:16 +0000

    chore: added pagination to page

--- orders/orders.js
+++ orders/orders.js
@@ -1,3 +1,5 @@
+const contentId = "content";
+//
 const orders = [
   {
     name: "Item 1",
@@ -29,42 +31,312 @@ const orders = [
     price: "$ 60.10",
     status: "Shipped",
   },
+  {
+    name: "Item 6",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 7",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 8",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 9",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Delivered",
+  },
+  {
+    name: "Item 10",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 11",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 12",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 13",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 14",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Delivered",
+  },
+  {
+    name: "Item 15",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 16",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 17",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 18",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 19",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Delivered",
+  },
+  {
+    name: "Item 20",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 21",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 22",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 23",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 24",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Delivered",
+  },
+  {
+    name: "Item 25",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 26",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 27",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 28",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 29",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Delivered",
+  },
+  {
+    name: "Item 30",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 31",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 32",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Processing",
+  },
+  {
+    name: "Item 33",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
+  {
+    name: "Item 34",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Delivered",
+  },
+  {
+    name: "Item 35",
+    date: "20th October, 2021",
+    price: "$ 60.10",
+    status: "Shipped",
+  },
 ];
-const contentId = "content";
-var pagination = { start: 0, end: 2 };
 
-function suspendPageToLoading(contentId) {
+/**
+ * @TABLEINTERFACE
+ */
+
+const OrdersTableInterface = {
+  id: "orders-table",
+
+  getTableBodyElement: function () {
+    return document.querySelector(`#${OrdersTableInterface.id}>tbody`);
+  },
+
+  clearTableOrders: function () {
+    const tableBody = this.getTableBodyElement();
+    tableBody.innerHTML = "";
+  },
+
+  createPageOrders: function (orders) {
+    const tableBody = this.getTableBodyElement();
+    let bodyHTML = "";
+    orders.forEach((order) => {
+      const orderHTML = `
+      <tr>
+      <td>${order.name}</td>
+      <td>${order.date}</td>
+      <td>${order.price}</td>
+      <td class="${order.status.toLowerCase()}" >${order.status}</td>
+      </tr>
+      `;
+      bodyHTML += orderHTML;
+    });
+    tableBody.innerHTML = bodyHTML;
+  },
+
+  toNextPage: function (currentPaginationConfig, orders) {
+    const { start, end } = currentPaginationConfig;
+    const updatedPagination = {
+      ...currentPaginationConfig,
+      start: start < end ? start + 1 : end,
+    };
+    this.updatePageOrdersAndPagination(orders, updatedPagination);
+  },
+
+  toPreviousPage: function (currentPaginationConfig, orders) {
+    const { start } = currentPaginationConfig;
+    if (start === 1) return;
+    const updatedPagination = {
+      ...currentPaginationConfig,
+      start: start - 1,
+    };
+    this.updatePageOrdersAndPagination(orders, updatedPagination);
+  },
+
+  updatePageOrdersAndPagination: function (orders, updatedPagination) {
+    const { start: newStart, itemsPerPage } = updatedPagination;
+    const pageOrders = orders.slice(
+      newStart * itemsPerPage - itemsPerPage,
+      newStart * itemsPerPage
+    );
+    OrdersTableInterface.clearTableOrders();
+    this.createPageOrders(pageOrders);
+    this.setPaginationIndexes(updatedPagination);
+    this.setPaginationConfiguration(updatedPagination);
+  },
+
+  setPaginationConfiguration: function (paginationConfig) {
+    window.pagination = paginationConfig;
+  },
+
+  initPaginationConfig: function (orders) {
+    const itemsPerPage = 10;
+    var pagination = {
+      itemsLength: orders.length,
+      itemsPerPage,
+      start: 1,
+      end: Math.ceil(orders.length / itemsPerPage),
+    };
+    this.setPaginationConfiguration(pagination);
+    this.setPaginationIndexes(pagination);
+  },
+
+  setPaginationIndexes: function (paginationConfig) {
+    const startElement = document.getElementById("pagination-start");
+    startElement.innerText = paginationConfig.start;
+    const endElement = document.getElementById("pagination-end");
+    endElement.innerText = paginationConfig.end;
+  },
+};
+
+/**
+ *
+ * @PAGEMETHODS
+ */
+
+function suspendPageToLoadingState(contentId) {
   const content = document.getElementById(contentId);
   content.className = "hide";
 }
 
-function loadOrders(orders) {
+function loadFirstPageOrders(orders) {
   const content = document.getElementById("content");
-  content.className = "";
-  const ordersTable = document.getElementById("orders-table");
-  orders.forEach((order) => {
-    const row = ordersTable.insertRow(-1);
-    const nameCell = row.insertCell(0);
-    const dateCell = row.insertCell(1);
-    const priceCell = row.insertCell(2);
-    const statusCell = row.insertCell(3);
+  const firstPageOrders = orders.slice(0, 10);
+  OrdersTableInterface.createPageOrders(firstPageOrders);
+  content.className = ""; // reset content class prop to initiate id style
+}
 
-    nameCell.appendChild(document.createTextNode(order.name));
-    dateCell.appendChild(document.createTextNode(order.date));
-    priceCell.appendChild(document.createTextNode(order.price));
-    statusCell.appendChild(document.createTextNode(order.status));
+function nextOrdersPage() {
+  const currentPaginationConfig = window.pagination;
+  OrdersTableInterface.toNextPage(currentPaginationConfig, orders);
+}
 
-    statusCell.className = order.status.toLowerCase();
-  });
+function previousOrdersPage() {
+  const currentPaginationConfig = window.pagination;
+  OrdersTableInterface.toPreviousPage(currentPaginationConfig, orders);
 }
 
 async function loadPage() {
-  suspendPageToLoading(contentId);
+  suspendPageToLoadingState(contentId);
   await (function () {
     setTimeout(() => {
-      loadOrders(orders);
+      OrdersTableInterface.initPaginationConfig(orders);
+      loadFirstPageOrders(orders);
     }, 2000);
   })();
 }
 
+// Load Page
 loadPage();
